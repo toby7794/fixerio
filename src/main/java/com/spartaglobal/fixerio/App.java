84a939a4c3f352a204cd0d1463d40a15e074dcb7
T L Goddard <TGoddard@spartaglobal.com>
2019-04-25 11:51:24 +0100

    initial commit

---
+++ src/main/java/com/spartaglobal/fixerio/App.java
@@ -0,0 +1,13 @@
+package com.spartaglobal.fixerio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
